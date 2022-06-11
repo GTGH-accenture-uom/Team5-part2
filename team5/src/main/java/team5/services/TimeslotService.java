760693a82a5fdc2cad35dea1cd8399090ccfea96
rouva <p16rouv@ionio.gr>
2022-06-11 13:18:59 +0300

    add Timeslotservice #13

---
+++ team5/src/main/java/team5/services/TimeslotService.java
@@ -0,0 +1,45 @@
+package team5.services;
+
+import team5.model.Timeslot;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+
+public class TimeslotService {
+    private final List<Timeslot> allTimeslots = new ArrayList<>();
+
+
+    public void createTimeslot(LocalDateTime startDateTime, int duration){
+        Timeslot timeslot = new Timeslot(startDateTime, duration);
+        System.out.println("Timeslot with id: " + timeslot.getId() + " created: " + timeslot);
+        allTimeslots.add(timeslot);
+    }
+
+    public Timeslot findTimeslotById(int id) {
+        Timeslot foundTimeslot = null;
+        Optional<Timeslot> timeslot;
+        for (Timeslot t: allTimeslots){
+            if (t.getId()==id){
+                foundTimeslot = t;
+            }
+        }
+        return foundTimeslot;
+    }
+
+    public List<Timeslot> findTimeslotsByDate(LocalDate localDate){
+        List<Timeslot> timeslots = new ArrayList<>();
+        for (Timeslot t: allTimeslots){
+            if (t.isAvailable() && ((t.getStartDateTime().toLocalDate().equals(localDate)))){
+                timeslots.add(t);
+            }
+        }
+        return timeslots;
+    }
+
+
+}
